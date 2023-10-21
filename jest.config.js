a5014f65394497c627036684da1918c3fc0cfa73
Augusto Gomez <agomezsantos95@gmail.com>
2023-10-21 11:12:04 +0200

    Add testing suites for getPokemonPages

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,5 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   resolver: "jest-ts-webcompat-resolver",
+  testMatch: ["**/src/**/*.test.ts"],
 };
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,6 +1,11 @@
 import {
-  pokemonInfo,
+  getPokemonPages,
   sayPokemonNames,
 } from "./data/getPokemonPages/getPokemonPages.js";
+
+const firstPageUrl = "https://pokeapi.co/api/v2/pokemon?limit=10&offset=";
+const pageStartingPokemonUrl = "100";
+
+const pokemonInfo = await getPokemonPages(firstPageUrl, pageStartingPokemonUrl);
 
 sayPokemonNames(pokemonInfo);
