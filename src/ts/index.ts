0a551e6b1f442808ea773641a027b19fe19402f8
Augusto Gomez <agomezsantos95@gmail.com>
2023-10-21 17:23:38 +0200

    Add PokemonList component

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,21 +1,18 @@
-import {
-  getPokemonPages,
-  sayPokemonNames,
-} from "./data/getPokemonPages/getPokemonPages.js";
+import { getPokemonPages } from "./data/getPokemonPages/getPokemonPages.js";
 import App from "./Components/App/App.js";
-import PokemonItem from "./Components/PokemonItem/PokemonItem.js";
 
-const firstPageUrl = "https://pokeapi.co/api/v2/pokemon?limit=10&offset=";
+import PokemonList from "./Components/PokemonList/PokemonList.js";
+
+const firstPageUrl = "https://pokeapi.co/api/v2/pokemon?limit=20&offset=";
 const pageStartingPokemonUrl = "100";
 
 const pokemonInfo = await getPokemonPages(firstPageUrl, pageStartingPokemonUrl);
-
-sayPokemonNames(pokemonInfo);
 
 const body = document.querySelector("body")!;
 
 const app = new App(body);
 app.render();
-const frame = document.querySelector(".frame");
-const pokemonUnit = new PokemonItem(frame!, pokemonInfo.results[0]);
-pokemonUnit.render();
+const frame = document.querySelector(".frame")!;
+
+const pokemonList = new PokemonList(frame, pokemonInfo.results);
+pokemonList.render();
