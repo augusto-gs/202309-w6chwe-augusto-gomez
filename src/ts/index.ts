0cea0bfa11e0f6be2459aeb271e1983dccaa2b1e
Augusto Gomez <agomezsantos95@gmail.com>
2023-10-21 19:23:12 +0200

    Add button component to proyect

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,9 +1,8 @@
 import { getPokemonPages } from "./data/getPokemonPages/getPokemonPages.js";
 import App from "./Components/App/App.js";
-
 import PokemonList from "./Components/PokemonList/PokemonList.js";
 
-const firstPageUrl = "https://pokeapi.co/api/v2/pokemon?limit=20&offset=";
+const firstPageUrl = "https://pokeapi.co/api/v2/pokemon?limit=10&offset=";
 const pageStartingPokemonUrl = "100";
 
 const pokemonInfo = await getPokemonPages(firstPageUrl, pageStartingPokemonUrl);
