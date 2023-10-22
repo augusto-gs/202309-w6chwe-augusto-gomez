ef056a5dfc6c6da922a4b7ac1a899cf651f03194
Augusto Gomez <agomezsantos95@gmail.com>
2023-10-22 14:38:56 +0200

    Add next and previous button functionality to change pages

--- src/ts/Components/App/App.ts
+++ src/ts/Components/App/App.ts
@@ -1,7 +1,4 @@
 import Component from "../Component/Component.js";
-import Button from "../Button/Button.js";
-import { getPokemonPages } from "../../data/getPokemonPages/getPokemonPages.js";
-import PokemonList from "../PokemonList/PokemonList.js";
 
 class App extends Component {
   constructor(parentElement: Element) {
@@ -18,42 +15,6 @@ class App extends Component {
         </section>
       </main>
     `;
-
-    const nextButton = new Button(this.element, "next", "Next", async () => {
-      const currentPageUrl =
-        "https://pokeapi.co/api/v2/pokemon?limit=10&offset=";
-      let pageStartingPokemonUrl = 0;
-      const frame = this.deletePokemonList();
-      pageStartingPokemonUrl += 10;
-
-      const pokemonInfo = await getPokemonPages(
-        currentPageUrl,
-        pageStartingPokemonUrl,
-      );
-
-      const pokemonList = new PokemonList(frame, pokemonInfo.results);
-      pokemonList.render();
-    });
-    nextButton.render();
-
-    const previousButton = new Button(
-      this.element,
-      "previous",
-      "Previous",
-      () => {
-        console.log("");
-      },
-    );
-    previousButton.render();
-  }
-
-  private deletePokemonList() {
-    const frame = document.querySelector(".frame")!;
-    frame.querySelector("ul")?.remove();
-    frame?.querySelectorAll("li").forEach((listElement) => {
-      listElement.remove();
-    });
-    return frame;
   }
 }
 
